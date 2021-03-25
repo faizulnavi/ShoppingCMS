a49aaea3416e0328993cf2db76c1ca67ab04272f
faizulnavi <faizulnavi@yahoo.co.in>
2021-03-26 03:54:20 +0530

    Update MDF

---
+++ ShoppingCMS/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,88 @@
+﻿using ShoppingCMS.Models.Data;
+using ShoppingCMS.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoppingCMS.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Admin/Pages
+        public ActionResult Index()
+        {
+            //Declare List of PageVM
+            List<PageVM> pagesList;
+
+            using (Db db = new Db())
+            {
+                //Init the list
+                pagesList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+            }
+
+            //Return the view List
+            return View(pagesList);
+        }
+        // GET: Admin/Pages/AddPage
+        [HttpGet]
+        public ActionResult AddPage()
+        {
+            return View();
+        }
+        // Post: Admin/Pages/AddPage
+        [HttpPost]
+        public ActionResult AddPage(PageVM model)
+        {
+            //Check Model state
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            using (Db db = new Db())
+            {
+
+                //Declare slug
+                string slug;
+
+                //Init PageDTO
+                PageDTO dto = new PageDTO();
+
+                //DTO Title
+                dto.Title = model.Title;
+
+                //Check for and set slug if need be
+                if (string.IsNullOrWhiteSpace(model.Slug))
+                {
+                    slug = model.Title.Replace(" ", "-").ToLower();
+                }
+                else
+                {
+                    slug = model.Slug.Replace(" ", "-").ToLower();
+                }
+
+                //Make sure title and slug are unique
+                if(db.Pages.Any(x => x.Title == model.Title) || db.Pages.Any (x => x.Slug == model.Slug))
+                {
+                    ModelState.AddModelError("", "Title or Slug already exist");
+                    return View(model);
+                }
+                //DTO the rest
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.HasSidebar = model.HasSidebar;
+                dto.Sorting = 100;
+
+                //Save DTO
+                db.Pages.Add(dto);
+                db.SaveChanges();
+            }
+
+            //Save TempData Message
+            TempData["SM"] = "You have added new Page";
+            //Redirect
+            return RedirectToAction("AddPage");
+        }
+    }
+}
