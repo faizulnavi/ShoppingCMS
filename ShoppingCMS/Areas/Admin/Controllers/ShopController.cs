4e80c1e7012ed261cc0401af9d94cd60da912c10
Faizul Navi Khan <faizulnavi@yahoo.co.in>
2021-06-07 00:08:00 +0200

    commit with new machine.

---
+++ ShoppingCMS/Areas/Admin/Controllers/ShopController.cs
@@ -0,0 +1,31 @@
+﻿using ShoppingCMS.Models.Data;
+using ShoppingCMS.Models.ViewModels.Shop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoppingCMS.Areas.Admin.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Admin/Shop/Categories
+        public ActionResult Categories()
+        {
+            //Declare a list of Models
+            List<CategoryVM> categoryVMList;
+            using (Db db = new Db())
+            {
+                //Init the list
+                categoryVMList = db.Categories
+                                 .ToArray()
+                                 .OrderBy(x => x.Sorting)
+                                 .Select(x => new CategoryVM(x))
+                                 .ToList();
+            }
+            //return the list
+            return View(categoryVMList);
+        }
+    }
+}
